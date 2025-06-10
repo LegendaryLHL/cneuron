3a178056fd511c421fc7907d74625d57b56e9177
BrBa2 <ddddddddrrdd3@gmail.com>
2025-06-10 17:39:57 +0800

    Fix: add back test

--- test/linear_algebra.cpp
+++ test/linear_algebra.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "test_utils.h"
+
 extern "C" {
 #include "cneuron/cneuron.h"
 }
@@ -23,4 +25,56 @@ TEST(LinearAlgebraTest, MatrixMultiply) {
 
     float *c = (float *)malloc(sizeof(float) * rows_a * cols_b);
     matrix_vector_multiply(a, b, c, rows_a, cols_a);
+    ASSERT_FLOAT_EQ(c[0], 4.0f);
+    ASSERT_FLOAT_EQ(c[2], 12.0f)
+    ASSERT_FLOAT_EQ(c[4], 10.0f);
+    ASSERT_FLOAT_EQ(c[7], 12.0f);
+
+    free(a);
+    free(b);
+    free(c);
+}
+
+TEST(LinearAlgebraTest, VectorApplyActivation) {
+    size_t length = 3;
+    float *a = (float *)malloc(sizeof(float) * length);
+    float *b = (float *)malloc(sizeof(float) * length);
+
+    a[0] = 1.0;
+    a[1] = -1.2;
+    a[2] = -0.2;
+
+    vector_apply_activation(a, b, length, sigmoid);
+
+    for (size_t i = 0; i < length; i++) {
+        ASSERT_FLOAT_EQ(b[i], sigmoid(a[i], false));
+    }
+
+    free(a);
+    free(b);
+}
+
+TEST(LinearAlgebraTest, VectorAdd) {
+    size_t length = 3;
+    float *a = (float *)malloc(sizeof(float) * length);
+    float *b = (float *)malloc(sizeof(float) * length);
+    float *c = (float *)malloc(sizeof(float) * length);
+
+    a[0] = 1.0;
+    a[1] = -1.2;
+    a[1] = -0.2;
+
+    b[0] = 0.2;
+    b[1] = -0.2;
+    b[2] = 0.6;
+
+    vector_add(a, b, c, length);
+
+    for (size_t i = 0; i < length; i++) {
+        ASSERT_FLOAT_EQ(c[i], a[i] + b[i]);
+    }
+
+    free(a);
+    free(b);
+    free(c);
 }
