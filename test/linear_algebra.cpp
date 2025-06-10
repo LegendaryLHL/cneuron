31a409ddba1e7372c47f7b50eba1f355da06b1bd
Lee Hoon Lim <hoonlim123@gmail.com>
2025-06-10 21:16:10 +0800

    Code coverage fr

--- test/linear_algebra.cpp
+++ test/linear_algebra.cpp
@@ -13,15 +13,39 @@ TEST(LinearAlgebraTest, VectorApplyActivation) {
     float *a = (float *)malloc(sizeof(float) * length);
     float *b = (float *)malloc(sizeof(float) * length);
 
-    a[0] = 1.0;
-    a[1] = -1.2;
-    a[2] = -0.2;
+    a[0] = 1.0f;
+    a[1] = -1.2f;
+    a[2] = -0.2f;
 
     vector_apply_activation(a, b, length, sigmoid, false);
 
     for (size_t i = 0; i < length; i++) {
         ASSERT_FLOAT_EQ(b[i], sigmoid(a[i], false));
     }
+
+    free(a);
+    free(b);
+}
+
+
+TEST(LinearAlgebraTest, HadamardProduct) {
+    size_t length = 3;
+    float *a = (float *)malloc(sizeof(float) * length);
+    float *b = (float *)malloc(sizeof(float) * length);
+
+    a[0] = 1.0f;
+    a[1] = -1.2f;
+    a[2] = -0.2f;
+
+    b[0] = 1.2f;
+    b[1] = -2.2f;
+    b[2] = -0.4f;
+
+    hadamard_product(a, b, b, length);
+
+    ASSERT_FLOAT_EQ(b[0], 1.2f);
+    ASSERT_FLOAT_EQ(b[1], 2.64f);
+    ASSERT_FLOAT_EQ(b[2], 0.08f);
 
     free(a);
     free(b);
