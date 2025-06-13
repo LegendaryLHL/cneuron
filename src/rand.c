75bc06a461e17f1cef88a3d15d223df67033231e
BrBa2 <ddddddddrrdd3@gmail.com>
2025-06-13 13:35:28 +0800

    Fix: prng_gen work at beginning

--- src/rand.c
+++ src/rand.c
@@ -1,11 +1,7 @@
-#include "rand.h"
-
 #include <assert.h>
-#include <stdlib.h>
 #include <string.h>
-#include "shishua/shishua-sse2.h"
 
-static prng_state __randstate;
+#include "rand.h"
 
 __attribute__((constructor)) static void auto_seed_rand(void) {
     static uint64_t seed[4] = {0x9E3779B97F4A7C15, 0xF39CC0605CEDC834, 0x1082276BF3A27251, 0xF86C6A11D0C18E95};
@@ -14,37 +10,38 @@ __attribute__((constructor)) static void auto_seed_rand(void) {
 }
 
 uint8_t randnum_u8(struct rand_chunk *randc, uint8_t range, uint8_t offset) {
-    assert(max > min);
-    assert(randc->count <= 1023);
-    uint8_t randnum = ((uint16_t)randc->buf[randc->count] * range) >> 8;
-    randnum += offset;
-    randc->count++;
+    assert(range);
     if (randc->count > 1023) {
 	randc->count = 0;
 	prng_gen(&__randstate, randc->buf, 1024);
     }
+    uint8_t randnum = ((uint16_t)randc->buf[randc->count] * range) >> 8;
+    randnum += offset;
+    randc->count++;
     return randnum;
 }
 
 uint32_t randnum_u32(struct rand_chunk *randc, uint32_t range, uint32_t offset) {
-    assert(max > min);
-    assert(randc->count <= 1020);
+    assert(range);
+    if (randc->count > 1020) {
+	randc->count = 0;
+	prng_gen(&__randstate, randc->buf, 1024);
+    }
     uint32_t value;
     // uint32_t value = randc->buf[randc->count++] << 24 | randc->buf[randc->count++] << 16 | randc->buf[randc->count++] << 8 | randc->buf[randc->count++];
     memcpy(&value, &randc->buf[randc->count], sizeof(value));
     uint32_t randnum = ((uint64_t)value * range) >> 32;
     randnum += offset;
     randc->count += sizeof(randnum);
-    if (randc->count > 1020) {
-	randc->count = 0;
-	prng_gen(&__randstate, randc->buf, 1024);
-    }
     return randnum;
 }
 
 float randf(struct rand_chunk *randc, float range, float offset) {
     assert(range);
-    assert(randc->count <= 1021);
+    if (randc->count > 1020) {
+	randc->count = 0;
+	prng_gen(&__randstate, randc->buf, 1024);
+    }
     // uint32_t value;
     // memcpy(&value, &randc->buf[randc->count], 4);
     uint32_t value = randc->buf[randc->count++] << 16 | randc->buf[randc->count++] << 8 | randc->buf[randc->count++];
@@ -54,10 +51,6 @@ float randf(struct rand_chunk *randc, float range, float offset) {
     // memcpy(&randfloat, &value, sizeof(randfloat));
     // randfloat = (randfloat - 1) * range + offset;
     float randfloat = (value) / 16777216.0f * range + offset;
-    if (randc->count > 1021) {
-	randc->count = 0;
-	prng_gen(&__randstate, randc->buf, 1024);
-    }
     return randfloat;
 }
 
--- src/rand.h
+++ src/rand.h
@@ -7,13 +7,17 @@
 #include "../external/shishua/shishua.h"
 
 struct rand_chunk {
-    size_t count __attribute__((aligned(32)));
-    uint8_t buf[1024] __attribute__((aligned(32)));// NOTE: must be multiple of 256
+    size_t count;
+    uint8_t buf[1024];// NOTE: must be multiple of 256
 };
 
 uint8_t randnum_u8(struct rand_chunk *randc, uint8_t range, uint8_t offset);
 uint32_t randnum_u32(struct rand_chunk *randc, uint32_t range, uint32_t offset);
 float randf(struct rand_chunk *randc, float range, float offset);
-static struct rand_chunk randc = {0};
+static struct rand_chunk randc = {
+  .count = 2000,
+  .buf = {0}
+};
 
+static prng_state __randstate;
 #endif
